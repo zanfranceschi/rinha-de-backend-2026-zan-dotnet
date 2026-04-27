e3943803a54e4f3139cab86dedb7fbd98e65f2d4
Francisco Zanfranceschi <zan@cora.com.br>
2026-04-27 12:25:28 -0300

    wip

--- containerization/build-and-publish.sh
+++ containerization/build-and-publish.sh
@@ -5,6 +5,7 @@ IMAGE="zanfranceschi/rinha-de-backend-2026-zan-dotnet"
 TAG=$(date +%Y%m%d%H%M)
 
 docker build \
+    --platform linux/amd64 \
     -t "$IMAGE:$TAG" \
     -t "$IMAGE:latest" \
     -t "rinha-de-backend-2026-zan-dotnet:latest" \
